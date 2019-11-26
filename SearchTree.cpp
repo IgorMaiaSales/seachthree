e7b17b6b5ea3f6899046ff38ac59e2a59dac1bcd
Igor <alunom33@acad.unifor.br>
2019-11-26 10:05:22 -0300

    Balanceamento da arv Ternária

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -60,10 +60,78 @@ void addToThree(int type, Generic* avr, const char* word){
 void printTree(Ternary *arv){
     if(arv == NULL) return;
 
-    printf("%c \n", arv->c);
+    printf("%c ", arv->c);
     printTree(arv->left);
     printTree(arv->mid);
     printTree(arv->right);
+}
+
+int arvProfundidade(Ternary *a){
+    if(a == NULL) return -1;
+    if(a->left == NULL && a->right == NULL){
+        return 0;
+    }else{
+        if(arvProfundidade(a->left) > arvProfundidade(a->right)) return 1 + arvProfundidade(a->left);
+        else return 1 + arvProfundidade(a->right);
+    }
+}
+
+int arvBalanceamento(Ternary *a){
+    if(a == NULL) return 0;
+    if(a->left == NULL && a->right == NULL) return 0;
+    return arvProfundidade(a->left) - arvProfundidade(a->right);
+}
+
+void arvRotDir(Ternary **a){
+    Ternary *tmp1, *tmp2;
+    tmp1 = (*a)->left;
+    tmp2 = tmp1->right;
+    tmp1->right= *a;
+    (*a)->left = tmp2;
+    *a=tmp1;
+}
+
+void arvRotEsq(Ternary **a){
+    Ternary *tmp1, *tmp2;
+    tmp1 = (*a)->right;
+    tmp2 = tmp1->left;
+    tmp1->left = *a;
+    (*a)->right = tmp2;
+    *a=tmp1;
+}
+
+void arvRotaciona(Ternary **a){
+    if(a == NULL) return;
+
+    if(arvBalanceamento(*a) > 1){
+        if(arvBalanceamento((*a)->left) < 0) arvRotEsq(&(*a)->left);
+        arvRotDir(&(*a));
+    }else{
+        if(arvBalanceamento((*a)->right) > 0) arvRotDir(&(*a)->right);
+        arvRotEsq(&(*a));
+    }
+}
+
+Ternary **arvBuscaDesb(Ternary **a){
+    if(*a == NULL) return NULL;
+
+    if((*a)->left != NULL){
+        Ternary **tmp = arvBuscaDesb(&(*a)->left);
+        if(tmp != NULL) return tmp;
+    }
+    if((*a)->right != NULL){
+        Ternary **tmp = arvBuscaDesb(&(*a)->right);
+        if(tmp != NULL) return tmp;
+    }
+
+    if(arvBalanceamento(*a) > 1 || arvBalanceamento(*a) < -1) return a;
+
+    if((*a)->mid != NULL){
+        Ternary **tmp = arvBuscaDesb(&(*a)->mid);
+        if(tmp != NULL) return tmp;
+    }
+
+    return NULL;
 }
 
 int main()
@@ -79,6 +147,11 @@ int main()
             addToThree(0, &myArv, line.c_str());
         }
     }
+
+    printTree(myArv.avrTernary);
+    cout << endl;
+
+    while(arvBuscaDesb(&myArv.avrTernary)!=NULL) arvRotaciona(arvBuscaDesb(&myArv.avrTernary));
 
     printTree(myArv.avrTernary);
 
